2c3881093a0462b942cf7832dc4831b1fed6f8b5
Fedor Khan <fedor.khan41@yandex.ru>
2024-12-29 23:27:39 +0300

    main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from src.masks import get_mask_account, get_mask_card_number
+from src.widget import get_date, mask_account_card
 
-print(get_mask_card_number(1234567812345678))
-print(get_mask_account(341769795790278))
+
+print(get_date("2024-03-11T02:26:18.671407"))
+print(mask_account_card("Счет 64686473678894779589"))
+print(mask_account_card("Visa Gold 5999414228426353"))
+print(mask_account_card("Maestro 1596837868705199"))
