7bd34c2dc9623a0b5cbbda6919531645082d3569
Fedor Khan <fedor.khan41@yandex.ru>
2025-01-04 12:12:59 +0300

    fixes to imports in main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from src.widget import get_date, mask_account_card
+from src import widget
 
 
-print(get_date("2024-03-11T02:26:18.671407"))
-print(mask_account_card("Счет 64686473678894779589"))
-print(mask_account_card("Visa Gold 5999414228426353"))
-print(mask_account_card("Maestro 1596837868705199"))
+print(widget.get_date("2024-03-11T02:26:18.671407"))
+print(widget.get_date("Счет 64686473678894779589"))
+print(widget.mask_account_card("Visa Gold 5999414228426353"))
+print(widget.mask_account_card("Maestro 1596837868705199"))
