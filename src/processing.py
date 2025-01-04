3077094f571abaf3b101ee3adee8fc4f4caff430
Fedor Khan <fedor.khan41@yandex.ru>
2025-01-04 14:09:55 +0300

    added processing module

---
+++ src/processing.py
@@ -0,0 +1,25 @@
+def filter_by_state(operations_list: list[dict], state: str = 'EXECUTED') -> list[dict]:
+    """
+    Принимает список словарей и опционально значение для ключа state (по умолчанию 'EXECUTED')
+    Возвращает новый список словарей, содержащий только те словари,
+    у которых ключ state соответствует указанному значению
+    :param operations_list: list[dict]
+    :param state: str
+    :return: filtered_operations_list -> list[dict]
+    """
+    filtered_operations_list = []
+
+    for operation in operations_list:
+        if operation['state'] == state:
+            filtered_operations_list.append(operation)
+
+    return filtered_operations_list
+
+
+if __name__ == '__main__':
+    check_operation_list = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+    print(f"Успешные: {filter_by_state(check_operation_list)}")
+    print(f"Отмененные: {filter_by_state(check_operation_list, state='CANCELED')}")
